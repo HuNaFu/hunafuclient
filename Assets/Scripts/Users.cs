e30c51d52c4afe81c00cd69279c470037e4baf03
nihits <nihits@yahoo.com>
2015-05-12 12:52:25 -0700

    resolving the authentication problem.

--- Assets/Scripts/Users.cs
+++ Assets/Scripts/Users.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
+using System.Collections.Generic;
 
-public class Users : MonoBehaviour {
-
-	public Text ShowUsers;
+public class Users : MonoBehaviour
+{
+	public Text ConnectedStatus;
+	public Text FoundUsersStatus;
+	public string ConnectURL;
 	public string UsersURL;
+	private Dictionary<string,string> ConnectionResponseHeader = null;
 
 	// Use this for initialization
 	void Start ()
 	{
-
-
-
-		StartCoroutine(CallURL(UsersURL,FoundUsers));
+		StartCoroutine(CallURL(ConnectURL,Connected));
 	}
 	
 	// Update is called once per frame
@@ -23,21 +24,34 @@ public class Users : MonoBehaviour {
 	
 	}
 
+	void Connected(string text)
+	{
+		ConnectedStatus.text = text;
+		StartCoroutine(CallURL(UsersURL,FoundUsers));
+	}
+	
 	void FoundUsers(string text)
 	{
-		ShowUsers.text = text;
+		FoundUsersStatus.text = text;
 	}
 
 	private IEnumerator CallURL(string url, Action<string> callback)
 	{
-		var www = new WWW(url);
+		var www = new WWW(url,null,ConnectionResponseHeader);
 		yield return www;
-		if (www.error == null)
+		if (www.error != null)
 		{
 			Debug.LogError("Error " + www.error + "\nLoading URL " + www.url);
 		}
 		else
 		{
+			if (ConnectionResponseHeader == null && www.responseHeaders != null && www.responseHeaders["SET-COOKIE"] != null)
+			{
+				ConnectionResponseHeader = new Dictionary<string, string>();
+				ConnectionResponseHeader["COOKIE"] = www.responseHeaders["SET-COOKIE"];
+			}
+			Debug.Log(" Text " + www.text);
+
 			callback(www.text);
 		}
 	}
